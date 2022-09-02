d2da7da705f51cd2a184325c929add7ff1bb36b4
Patricia Ho <hopatricia1711@gmail.com>
2022-09-03 00:45:21 +0700

    welcome section (button still on progress)

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,12 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
 });
+
+const navbar = document.getElementById('nav');
+window.onscroll = () => {
+    if (window.scrollY >= 100) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
